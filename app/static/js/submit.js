b755ff40ac9fe9bb683b612f2243407c43ca0a1c
Heaven <gilmarheavensilva@hotmail.com>
2017-11-01 17:58:47 -0200

    :hammer:
    :hammer:

--- app/static/js/submit.js
+++ app/static/js/submit.js
@@ -1,24 +1,3 @@
 $("#flag-submission").click(function() {
-
-    var cat = $(".task-box").data("category");
-    var score = $(".task-box").data("score");
-    var flag = $("#send-flag").val();
-
-    console.log("/submit/" + cat + "/" + score + "/" + btoa(flag));
-
-    $.ajax({
-        url: "/submit/" + cat + "/" + score + "/" + btoa(flag)
-    }).done(function(data) {
-
-        console.log(data);
-
-        if (data["success"]) {
-            $("#flag-input").val($(".lang").data("success"));
-            $("#flag-submission").removeClass("btn-primary");
-            $("#flag-submission").addClass("btn-success");
-            $("#flag-submission").attr('disabled','disabled');
-        } else {
-            $("#flag-input").val($(".lang").data("failure"));
-        }
-    });
+  $(".btn-danger").removeClass("btn-danger");
 });
---
+++ notifications.py
@@ -0,0 +1,3 @@
+FLAG_SUCCESS = ('God job, Valid flag!')
+FLAG_SUBMITTED_ALREADY = ('You already solved that problem!')
+FLAG_INCORRECT = ("Incorrect flag!")
