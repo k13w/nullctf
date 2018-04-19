10f0ce2b2819782807ff9e888c687f72bdd12727
Heaven <gilmarheavensilva@hotmail.com>
2018-04-19 13:06:38 -0300

    :diamonds:
    ...

--- app/__init__.py
+++ app/__init__.py
@@ -4,14 +4,18 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from flask_login import LoginManager
 from flask_dance.contrib.github import make_github_blueprint
+from configparser import ConfigParser
 
 app = Flask(__name__)
 app.config.from_object('config')
 app.secret_key = '04uth'
 
+cp = ConfigParser()
+cp.read_file(open('api.ini'))
+
 blueprint = make_github_blueprint(
-    client_id = '9731e7aa2917cc2a6d04',
-    client_secret = 'f55e7049f045d8363b165658b1ab570a0b69d8b3',
+    client_id = cp['API']['client_id'],
+    client_secret = cp['API']['client_secret'],
 )
 
 db = SQLAlchemy(app)
